7a06542ed3ce36e0f8ac68481f90dff5cb9bcd98
Brian Quinn <briandquinn@gmail.com>
2014-06-19 20:34:39 +0100

    Push Shipments

--- config/initializers/wombat.rb
+++ config/initializers/wombat.rb
@@ -2,7 +2,7 @@ Spree::Wombat::Config.configure do |config|
   config.connection_token = ENV["HUB_TOKEN"]
   config.connection_id = ENV["HUB_STORE_ID"]
 
-  config.push_objects = ["Spree::Order"]
+  config.push_objects = ["Spree::Order", "Spree::Shipment"]
   config.payload_builder = {
     "Spree::Order" => {:serializer => "DemoOrderSerializer", :root => "orders"}
   }
