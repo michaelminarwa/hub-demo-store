9f4c5287915340ba71f5c163c0c46c3a8a1cfa11
Richard Lister <rlister@gmail.com>
2014-06-20 15:39:50 -0400

    turn on static assets since rails needs to serve all assets from docker container

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,9 @@ HubDemoStore::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # config.serve_static_assets = false
+  ## turn this on for running inside a docker container as nginx is external
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
