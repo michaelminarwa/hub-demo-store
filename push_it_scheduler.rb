bd34cc670243808a573e12d720c4266560bd7aab
Richard Lister <rlister@gmail.com>
2014-07-08 10:11:10 -0400

    lengthen push_it perdiod to 30s default and set from env var

--- push_it_scheduler.rb
+++ push_it_scheduler.rb
@@ -6,7 +6,9 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '10s' do
+period = ENV['PUSH_IT_PERIOD'] || '30s'
+
+scheduler.every period, :overlap => false do
   system 'rake wombat:push_it'
 end
 
